37b5a5b4a1f9d8c094537627e5c3923391af14df
Bruno Barbosa <brworkit@gmail.com>
2020-09-30 14:45:30 -0300

    test

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mtools",
+    name="brworkit",
     version="0.0.1",
     author="brworkit",
     author_email="brworkit@gmail.com",
